8f877e95d18c66c4ede07fdf9b071d6a6b5a3700
Ekroner <413567383@qq.com>
2025-04-20 15:56:15 +0800

    feat: 增加了自定义RPC消息结构，提供了相关枚举类

---
+++ rpc-core/src/main/java/com/ekroner/rpc/protocol/ProtocolConstant.java
@@ -0,0 +1,22 @@
+package com.ekroner.rpc.protocol;
+
+/**
+ * 协议常量
+ */
+public class ProtocolConstant {
+
+    /**
+     * 消息头长度
+     */
+    int MESSAGE_HEADER_LENGTH = 17;
+
+    /**
+     * 协议魔数
+     */
+    byte PROTOCOL_MAGIC = 0x1;
+
+    /**
+     * 版本号
+     */
+    byte PROTOCOL_VERSION = 0x1;
+}
